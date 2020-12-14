b292e3691af6d7cbfd9b90017583b7894186a69b
Igor Woźniak <igor91wozniak@gmail.com>
2020-12-14 16:51:04 +0100

    changes in functions

--- js/script.js
+++ js/script.js
@@ -1,18 +1,6 @@
 { 
-    function playGame(playerInput){
-    clearMessages()
-    
 
-    function getMoveName(randomNumber){
-        if(randomNumber == 1){
-            return 'kamień';
-        } else if (randomNumber == 2){
-            return  'papier';
-        } else if (randomNumber == 3){
-            return  'nożyce';
-        }
-    }
-    function displayResult (computerMove, playerMove) { 
+    const displayResult = function (computerMove, playerMove) { 
         console.log('moves:', computerMove, playerMove);
         if (computerMove == 'kamień' && playerMove == 'papier'){
             printMessage('Ty wygrywasz!');
@@ -34,26 +22,33 @@
             printMessage('Remis!');
         }
     } 
+
+    const getMoveName = function (randomNumber){
+        if(randomNumber == 1){
+            return 'kamień';
+        } else if (randomNumber == 2){
+            return  'papier';
+        } else if (randomNumber == 3){
+            return  'nożyce';
+        }
+    }
+
+
+    const playGame = function (playerInput){
+    clearMessages()
     const randomNumber = Math.floor(Math.random() * 3 + 1);
     console.log('Wylosowana liczba to: ' + randomNumber);
     const computerMove = getMoveName(randomNumber);
-
-    
     printMessage('Mój ruch to: ' + computerMove);
-
-    
     console.log('Gracz wpisał: ' + playerInput);
     const playerMove = getMoveName(playerInput);
-
-    
     printMessage('Twój ruch to: ' + playerMove);
-   
     displayResult(computerMove, playerMove);
-}
+    }
 
-document.getElementById('play-rock').addEventListener('click', function(){
+    document.getElementById('play-rock').addEventListener('click', function(){
     playGame(1);
-  });
+    });
    
   document.getElementById('play-paper').addEventListener('click', function(){
     playGame(2);
